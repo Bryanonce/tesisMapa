84373e81f1c9fe91b7b0af25fcfbb03317f0068f
Bryan <bradcede@espol.edu.ec>
2020-11-02 15:16:29 -0500

    mapa de calor logrado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 //Servicios
 import { ApiRestService } from './servicios/api-rest.service';
-
 import { Component,OnInit } from '@angular/core';
 import { environment } from '../environments/environment';
 import * as mapboxgl from 'mapbox-gl';
@@ -12,51 +11,65 @@ import * as mapboxgl from 'mapbox-gl';
 })
 export class AppComponent implements OnInit{
   title = 'proyectoTesis';
-  mapa: mapboxgl.Map;
+  //mapa: mapboxgl.Map;
   public coorden = [];
   constructor(public datos: ApiRestService){
-  	this.datos.getDatosJson(environment.Apirest).subscribe((res:any)=>{
-  		res.usuarios.forEach((elemento)=>{
-  			this.coorden.push([elemento.lat,elemento.long]);
-  		})
-  		console.log(res.usuarios);
-  		console.log(this.coorden);
-  		/*for(let i=0;i<res.length;i++){
-  			this.coorden.push(res[i].lat,res[i].long);
-  		}*/
+	this.datos.getDatosJson(environment.Apirest).subscribe((res:any)=>{
+		res.usuarios.forEach((elemento)=>{
+		this.coorden.push(
+			{
+				"type": "Feature",
+				"properties": {},
+				"geometry": {
+					"type": "Point",
+					"coordinates": [elemento.long,elemento.lat]
+				}
+			}
+		);
+		})
 	});
   }
-  ngOnInit(){
-	this.mapa = new mapboxgl.Map({ 
-	accessToken: environment.claveMapbox,
-	container: 'mapa-mapbox', // container id
-	style: 'mapbox://styles/mapbox/streets-v11',
-	center: [-80.096961, -0.712307], // starting position
-	zoom: 15 // starting zoom
+  ngOnInit(){	
+	console.log(this.coorden);
+	var map = new mapboxgl.Map({ 
+		accessToken: environment.claveMapbox,
+		container: 'mapa-mapbox', // container id
+		style: 'mapbox://styles/mapbox/streets-v11',
+		center: [-80.096961, -0.712307], // starting position
+		zoom: 15 // starting zoom
 	})
-
-	this.mapa.on('load', function () {
-		this.mapa.addSource('some id', 
-		{
+	console.log("Mapa cargando .....")
+	map.on('load',()=>{
+		console.log(this.coorden);
+		map.addSource('earthquakes', {
 			type: 'geojson',
 			data: {
 				"type": "FeatureCollection",
-				"features": [{
-					"type": "Feature",
-					"properties": {},
-					"geometry": {
-						"type": "Point",
-						"coordinates": this.coorden
-					}
-				}]
+				"features": this.coorden
+			}
+		});
+		map.addLayer({
+			id: 'trees-point',
+			type: 'heatmap',
+			source: 'earthquakes',
+			paint: {
+				'heatmap-color': ['interpolate',['linear'],['heatmap-density'],
+					0,
+					'rgba(33,102,172,0)',
+					0.2,
+					'rgb(103,169,207)',
+					0.4,
+					'rgb(209,229,240)',
+					0.6,
+					'rgb(253,219,199)',
+					0.8,
+					'rgb(239,138,98)',
+					1,
+					'rgb(178,24,43)'
+					]
 			}
 		})
-	
-
+		
 	})
-	
   }
-
-
-
 }
